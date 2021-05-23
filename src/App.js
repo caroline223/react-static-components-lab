9738c7006d2a46b1bc88d1ce380e405ab6ae5c30
caroline223 <67647836+caroline223@users.noreply.github.com>
2021-05-23 14:37:48 -0400

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -7,10 +7,43 @@ class App extends Component {
 			<div className="App">
 				<CatComponent />
 				<GraceHopperQuoteComponent />
-				{/* one more component missing */}
+				<MouseComponent />
 			</div>
 		);
 	}
 }
 
 export default App;
+
+class CatComponent extends Component {
+	render() {
+		// your code in the return statement below!
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+}
+
+class GraceHopperQuoteComponent extends Component {
+	render() {
+		// your code in the return statement below!
+		return (
+			<div >
+				
+			</div>
+		);
+	}
+}
+
+class MouseComponent extends Component {
+	render() {
+		// your code in the return statement below!
+		return (
+			<div >
+				
+			</div>
+		);
+	}
+}
